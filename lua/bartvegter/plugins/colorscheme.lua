f802acc489a77e9bd41834ad35a79c0d11282416
Bart Vegter <accounts@bartvegter.com>
2025-12-23 02:02:41 +0100

    Added Monokai Pro Ristretto as default color scheme

--- lua/bartvegter/plugins/colorscheme.lua
+++ lua/bartvegter/plugins/colorscheme.lua
@@ -1,13 +1,40 @@
 return {
 	{
 		"ellisonleao/gruvbox.nvim",
-		enabled = true,
+		enabled = false,
 		priority = 1000,
 		config = function()
+			require("gruvbox").setup({
+				overrides = {
+					SignColumn = { bg = "#282828" },
+				},
+			})
 			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
-
+	{
+		"sainnhe/gruvbox-material",
+		enabled = false,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.gruvbox_material_enable_italic = true
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
+	{
+		"loctvl842/monokai-pro.nvim",
+		enabled = true,
+		priority = 1000,
+		config = function()
+			require("monokai-pro").setup({
+				filter = "ristretto", -- classic | octagon | pro | machine | ristretto | spectrum
+			})
+			vim.cmd.colorscheme("monokai-pro")
+		end,
+	},
 	{
 		"catppuccin/nvim",
 		enabled = false,
